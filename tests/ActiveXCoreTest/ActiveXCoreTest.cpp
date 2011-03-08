a2ab88ffeb8dc8b6a35eace14478bd1937105455
Jukka Ojanen <jukka.ojanen@kolumbus.fi>
2011-03-08 15:08:16 +0200

    Replaced CComBSTR with _bstr_t More const correctness Trimmed lines to 80

--- tests/ActiveXCoreTest/ActiveXCoreTest.cpp
+++ tests/ActiveXCoreTest/ActiveXCoreTest.cpp
@@ -14,26 +14,13 @@ Copyright 2009 Richard Bateman, Firebreath development team
 
 #include "win_common.h"
 #include "UnitTest++.h"
-#include "FactoryBase.h"
-#include <boost/make_shared.hpp>
 
-#define PRINT_TESTNAME  printf("Running unit test %s::%s...\n", UnitTestSuite::GetSuiteName(), \
-    m_details.testName); fflush(stdout)
+#define PRINT_TESTNAME  printf("Running unit test %s::%s...\n", \
+UnitTestSuite::GetSuiteName(), m_details.testName); fflush(stdout)
 
 #include "COMJavascriptObjectTest.h"
-
-using namespace std;
-
-namespace FB { namespace Npapi {
-    class NpapiPlugin;
-    class NpapiBrowserHost;
-    typedef boost::shared_ptr<NpapiBrowserHost> NpapiBrowserHostPtr;
-}; };
 
 int main()
 {
     return UnitTest::RunAllTests();
-
-    return 0;
 }
-
--- tests/ActiveXCoreTest/COMJavascriptObjectTest.h
+++ tests/ActiveXCoreTest/COMJavascriptObjectTest.h
@@ -19,41 +19,46 @@ Copyright 2009 Richard Bateman, Firebreath development team
 #include "variant_list.h"
 #include "COMJavascriptObject.h"
 #include "ActiveXBrowserHost.h"
+#include "FactoryBase.h"
 #include "TestJSAPI.h"
+
+// TODO(jtojanen): temporary addition, win_common.h is the right place for this
+#include <comdef.h>
 
 using namespace FB::ActiveX;
 
-
-DISPID getIDForName(IDispatchEx *obj, std::string name)
+DISPID getIDForName(IDispatchEx* object, const std::string& name)
 {
-    std::wstring wStr(FB::utf8_to_wstring(name));
-    CComBSTR nStr(wStr.c_str());
-    OLECHAR *oleStr = nStr.m_str;
-    DISPID dispId(-1);
-    HRESULT hr = obj->GetIDsOfNames(IID_NULL, &oleStr, 1, LOCALE_SYSTEM_DEFAULT, &dispId);
-    if (SUCCEEDED(hr)) {
-        return dispId;
-    } else {
-        return -1;
+    std::wstring tmp(FB::utf8_to_wstring(name));
+    OLECHAR* p = const_cast<OLECHAR *>(tmp.c_str());
+    DISPID dispId;
+    HRESULT hr = object->GetIDsOfNames(IID_NULL, &p, 1,
+        LOCALE_SYSTEM_DEFAULT, &dispId);
+    if (FAILED(hr)) {
+        return DISPID_UNKNOWN;
     }
+
+    BOOST_ASSERT(dispId != DISPID_UNKNOWN);
+    return dispId;
 }
 
-CComVariant InvokeMethod(IDispatchEx *obj, std::string methodName, VARIANT* args, size_t count)
+const _variant_t InvokeMethod(IDispatchEx* object,
+                              const std::string& methodName,
+                              VARIANT* args, size_t count)
 {
     DISPPARAMS params;
     params.cArgs = count;
     params.cNamedArgs = 0;
     params.rgvarg = args;
 
-    VARIANT res;
-    HRESULT hr = obj->Invoke(getIDForName(obj, methodName), IID_NULL, LOCALE_USER_DEFAULT,
-        DISPATCH_METHOD, &params, &res, NULL, NULL);
-
-    if (SUCCEEDED(hr)) {
-        return res;
-    } else {
+    _variant_t result;
+    HRESULT hr = object->Invoke(getIDForName(object, methodName), IID_NULL,
+        LOCALE_USER_DEFAULT, DISPATCH_METHOD, &params, &result, NULL, NULL);
+    if (FAILED(hr)) {
         throw FB::script_error("Could not get property");
     }
+
+    return result;
 }
 
 TEST (COMJavascriptObject_CreateTest)
@@ -63,7 +68,8 @@ TEST (COMJavascriptObject_CreateTest)
     ActiveXBrowserHostPtr host(new ActiveXBrowserHost(NULL, NULL));
 
     boost::shared_ptr<TestObjectJSAPI> testIf(new TestObjectJSAPI());
-    CComPtr<IDispatchEx> obj(getFactoryInstance()->createCOMJSObject(host, testIf));
+    CComPtr<IDispatchEx> obj(
+        getFactoryInstance()->createCOMJSObject(host, testIf));
 
     CHECK (true);
 }
@@ -75,7 +81,8 @@ TEST(COMJavascriptObject_Methods)
     ActiveXBrowserHostPtr host(new ActiveXBrowserHost(NULL, NULL));
 
     boost::shared_ptr<TestObjectJSAPI> testIf(new TestObjectJSAPI());
-    CComPtr<IDispatchEx> obj = getFactoryInstance()->createCOMJSObject(host, testIf);
+    CComPtr<IDispatchEx> obj(
+        getFactoryInstance()->createCOMJSObject(host, testIf));
 
     HRESULT hr = S_OK;
 
@@ -84,15 +91,15 @@ TEST(COMJavascriptObject_Methods)
 
     {
         try {
-            CComVariant args[2] = {"This is a test", 0};
+            _variant_t args[2] = {"This is a test", 0};
             InvokeMethod(obj, "setValue", args, 2);
             CHECK(true);
         } catch (...) {
             CHECK(false);
         }
         try {
-            CComVariant arg(0);
-            CComVariant ret = InvokeMethod(obj, "getValue", &arg, 1);
+            _variant_t arg(0);
+            _variant_t ret = InvokeMethod(obj, "getValue", &arg, 1);
 
             CW2A cStr(ret.bstrVal);
             std::string strRes(cStr);
@@ -105,11 +112,11 @@ TEST(COMJavascriptObject_Methods)
     try {
         // Test setting and then getting integer values
         for (int i = 0; i < TESTOBJECTJSAPI_ACCESSLISTLENGTH; i++) {
-            CComVariant args[2] = {i, i};
+            _variant_t args[2] = {i, i};
             InvokeMethod(obj, "setValue", args, 2);
 
-            CComVariant arg(i);
-            CComVariant ret = InvokeMethod(obj, "getValue", &arg, 1);
+            _variant_t arg(i);
+            _variant_t ret = InvokeMethod(obj, "getValue", &arg, 1);
             ret.ChangeType(VT_I4);
             CHECK(ret.lVal == i);
         }
@@ -120,53 +127,55 @@ TEST(COMJavascriptObject_Methods)
     try {
         // Test setting and then getting integer values
         for (int i = 0; i < TESTOBJECTJSAPI_ACCESSLISTLENGTH; i++) {
-            CComVariant args[2] = {(double)(i * 1.5), i};
+            _variant_t args[2] = {static_cast<double>(i * 1.5), i};
             InvokeMethod(obj, "setValue", args, 2);
 
-            CComVariant arg(i);
-            CComVariant ret = InvokeMethod(obj, "getValue", &arg, 1);
+            _variant_t arg(i);
+            _variant_t ret = InvokeMethod(obj, "getValue", &arg, 1);
             ret.ChangeType(VT_R8);
-            CHECK(ret.dblVal == (double)(i * 1.5));
+            CHECK(ret.dblVal == static_cast<double>(i * 1.5));
         }
     } catch (...) {
         CHECK(FALSE);
     }
 
     //// Test invalid parameters; this should *not* fire an exception *ever*
-    //try {
+    // try {
     //    CHECK(!host->Invoke(obj, STRID(host, "setValue"), NULL, 0, &res));
     //    CHECK(true);
-    //} catch (...) {
+    // } catch (...) {
     //    CHECK(false);
-    //}
+    // }
 
     //// Test invalid method name
-    //try {
-    //    CHECK(!host->Invoke(obj, STRID(host, "someMethodThatDoesntExist"), NULL, 0, &res));
+    // try {
+    //    CHECK(!host->Invoke(obj,
+    //     STRID(host, "someMethodThatDoesntExist"), NULL, 0, &res));
     //    CHECK(true);
-    //} catch (...) {
+    // } catch (...) {
     //    CHECK(false);
-    //}
+    // }
 }
 
 TEST(COMJavascriptObject_Properties)
 {
-    //PRINT_TESTNAME;
+    // PRINT_TESTNAME;
 
-    //NpapiPluginModule module;
-    //NpapiHost testHost(NULL, NULL, NULL);
-    //module.setNetscapeFuncs(testHost.getBrowserFuncs());
+    // NpapiPluginModule module;
+    // NpapiHost testHost(NULL, NULL, NULL);
+    // module.setNetscapeFuncs(testHost.getBrowserFuncs());
 
-    //NpapiBrowserHostPtr host = new NpapiBrowserHost(&module, testHost.getPluginInstance());
-    //host->setBrowserFuncs(testHost.getBrowserFuncs());
+    // NpapiBrowserHostPtr host =
+    //  new NpapiBrowserHost(&module, testHost.getPluginInstance());
+    // host->setBrowserFuncs(testHost.getBrowserFuncs());
 
-    //NPVariant res;
-    //NPVariant oneParam;
-    //boost::shared_ptr<TestObjectJSAPI> testIf = new TestObjectJSAPI();
-    //NPJavascriptObject *obj = NPJavascriptObject::NewObject(host, testIf);
+    // NPVariant res;
+    // NPVariant oneParam;
+    // boost::shared_ptr<TestObjectJSAPI> testIf = new TestObjectJSAPI();
+    // NPJavascriptObject *obj = NPJavascriptObject::NewObject(host, testIf);
 
     //// Test setting and getting many different datatypes with properties
-    //try {
+    // try {
     //    for (int i = 0; i < 10000; i++) {
     //        INT32_TO_NPVARIANT(i, oneParam);
     //        CHECK(host->SetProperty(obj, STRID(host, "value"), &oneParam));
@@ -179,22 +188,25 @@ TEST(COMJavascriptObject_Properties)
     //        CHECK(NPVARIANT_TO_DOUBLE(res) == (double)(i * 3.74));
 
     //        // Roundabout, but easy, way to convert this to a string =]
-    //        std::string testVal = FB::variant((double)(i * 653.251235)).convert_cast<std::string>();
+    //        std::string testVal =
+    //         FB::variant(
+    //          (double)(i * 653.251235)).convert_cast<std::string>();
     //        host->getNPVariant(&oneParam, FB::variant(testVal));
     //        CHECK(host->SetProperty(obj, STRID(host, "value"), &oneParam));
     //        CHECK(host->GetProperty(obj, STRID(host, "value"), &res));
     //        CHECK(testVal == res.value.stringValue.utf8characters);
     //    }
-    //} catch (...) {
+    // } catch (...) {
     //    CHECK(false);
-    //}
+    // }
 
     //// Test exception handling
-    //try {
-    //    CHECK(!host->GetProperty(obj, STRID(host, "SomePropertyThatDoesntExist"), &res));
+    // try {
+    //    CHECK(!host->GetProperty(obj,
+    //     STRID(host, "SomePropertyThatDoesntExist"), &res));
     //    CHECK(true);
-    //} catch (...) {
+    // } catch (...) {
     //    CHECK(false);
-    //}
+    // }
 }
 
